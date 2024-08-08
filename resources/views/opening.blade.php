ab6ec14174a5390fc38eff9cadce345a94fb89bc
ima-yusuke <yusuke1125aadxyz@gmail.com>
2024-08-08 10:36:16 +0900

    add:overflow-hidden追加

--- resources/views/opening.blade.php
+++ resources/views/opening.blade.php
@@ -1,16 +1,23 @@
 <x-template title="Birthday">
-    <div class="bg-test h-screen w-full flex flex-col items-center justify-center">
-        <img src="{{ asset('storage/img/top-flower.jpg') }}" class="h-[400px] w-[400px] object-cover rotate-infinite rounded-full">
-        <section class="hidden top-text absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-            <article class="text-top-black flex flex-col items-center gap-4">
-                <h1 class="text-2xl">Happy Birthday</h1>
-                <div class="flex flex-col items-center gap-1">
-                    <p class="text-base">Chihaya Takeuchi</p>
-                    <p>2024.08.18</p>
-                </div>
-            </article>
-        </section>
+    <div class="bg-test min-h-screen w-full flex flex-col items-center justify-center">
+        <!-- Image Container -->
+        <div class="relative h-[400px] w-[400px]">
+            <!-- Image -->
+            <img src="{{ asset('storage/img/top-flower.jpg') }}" class="h-full w-full object-cover rotate-infinite rounded-full">
+
+            <!-- Section to be centered -->
+            <section class="hidden top-text absolute inset-0 flex items-center justify-center">
+                <article class="text-top-black flex flex-col items-center gap-4">
+                    <h1 class="text-2xl">Happy Birthday</h1>
+                    <div class="flex flex-col items-center gap-1">
+                        <p class="text-base">Chihaya Takeuchi</p>
+                        <p>2024.08.18</p>
+                    </div>
+                </article>
+            </section>
+        </div>
     </div>
+
 
     <button type="button" id="open-birthday" class="hidden top-text hover:cursor-pointer text-top-black absolute bottom-6 left-1/2 transform -translate-x-1/2">NEXT →</button>
 
